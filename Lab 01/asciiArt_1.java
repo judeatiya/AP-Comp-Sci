765daa0c28de36aa8eaf3d2d237736f83db046f8
judeatiya <judeatiya@gmail.com>
2016-09-09 10:44:01 -0700

    Lab 01

---
+++ Lab 01/asciiArt_1.java
@@ -0,0 +1,8 @@
+public class asciiArt_1 {
+	public static void main(String[]args) {
+		System.out.println("         __   __   ____  ____  _  _   __  _  _  ____  ____ ");
+		System.out.println("	   _(  ) / _\\ (__  )(__  )( \\/ )_(  )/ )( \\(    \\(  __)");
+		System.out.println("	  / \\) \\/    \\ / _/  / _/  )  // \\) \\) \\/ ( ) D ( ) _) ");
+		System.out.println("	  \\____/\\_/\\_/(____)(____)(__/ \\____/\\____/(____/(____)");	                                                      
+      }
+}
---
+++ Lab 01/firstProg.java
@@ -0,0 +1,12 @@
+/*
+ * @Name: Jude Atiya
+ * @Period: 5
+ * @Date: 9/3/2016
+ */
+public class firstProg {
+	public static void main(String[]args ){
+		// My first program Hello World
+		System.out.println("Hello World");
+		System.out.println("\"Hello World\"");
+	}	
+}
